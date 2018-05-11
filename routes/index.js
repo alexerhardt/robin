d068f6dec28a547e70ecd2c5036af67ec5b8ad66
alex <alex.erhardt.es@gmail.com>
2018-05-11 12:34:40 +0200

    finish navbar doc and fix title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Robin - Home' });
 });
 
 router.get('/example', function(req, res, next) {
